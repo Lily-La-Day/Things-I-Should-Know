6604b8162df7ec0e903f6ecbfe7537e512405988
Lily Keeling <lily.keeling@gmail.com>
2019-08-27 09:35:28 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hola el mundo')
+
+
+const width = 1000
+
+const init = () => {
+
+  const makeGrid = () => {
+
+    for(let i = 0; i < width * width; i ++ ) {
+      const grid = document.querySelector('.grid')
+      const square = document.createElement('div')
+      grid.append(square)
+      square.classList.add('grid-item')
+
+
+    }
+  }
+  makeGrid()
+}
+
+
+window.addEventListener('DOMContentLoaded', init)
